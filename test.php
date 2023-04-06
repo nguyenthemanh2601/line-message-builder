273e24092fbf1c194020dc7cada1e5cae7edef32
Nguyen The Manh <hoangtupro.a4@gmail.com>
2023-04-06 15:32:06 +0700

    add test, remote xdebug 3

--- test.php
+++ test.php
@@ -6,3 +6,13 @@ $image = new ManhNt\Line\FlexMessage\Component\Image;
 $image->url('https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png');
 
 echo($image);
+echo "\n";
+echo "\n";
+
+$text = new ManhNt\Line\FlexMessage\Component\Text;
+$text->text('text')->wrap(true)->weight('bold')->size('xxl')->color('#ff0000')->decoration('underline')->style('italic')->flex(3)->action(
+    (new ManhNt\Line\FlexMessage\Component\Action\MessageAction)->text('action text')
+);
+echo($text);
+echo "\n";
+echo "\n";
