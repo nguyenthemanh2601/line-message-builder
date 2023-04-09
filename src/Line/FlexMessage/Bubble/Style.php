9248d4200d6640b50b14f00a08f0406e04053efa
Nguyen The Manh <hoangtupro.a4@gmail.com>
2023-04-09 20:52:07 +0700

    change Bubble Style

--- src/Line/FlexMessage/Bubble/Style.php
+++ src/Line/FlexMessage/Bubble/Style.php
@@ -2,25 +2,36 @@
 
 namespace ManhNt\Line\FlexMessage\Bubble;
 
+use ManhNt\Support\Container;
 use ManhNt\Contract\ArrayAble;
 use ManhNt\Line\FlexMessage\Component\BlockStyle;
 
 class Style implements ArrayAble
 {
-    public BlockStyle $header;
-    public BlockStyle $hero;
-    public BlockStyle $body;
-    public BlockStyle $footer;
+    public $header;
+    public $hero;
+    public $body;
+    public $footer;
 
-    public function __construct(BlockStyle $header, BlockStyle $hero, BlockStyle $body, BlockStyle $footer)
+    public function __construct(BlockStyle $header = null, BlockStyle $hero = null, BlockStyle $body = null, BlockStyle $footer = null)
     {
         $this->header = $header;
         $this->hero = $hero;
         $this->body = $body;
         $this->footer = $footer;
+        $this->initialize();
     }
 
-    public function toArray(): array
+    private function initialize()
+    {
+        foreach (get_object_vars($this) as $property => $value) {
+            if (!$this->{$property}) {
+                $this->{$property} = Container::getInstance()->make(BlockStyle::class);
+            }
+        }
+    }
+
+    public function toArray()
     {
         $value = [];
         foreach (get_object_vars($this) as $k => $v) {
